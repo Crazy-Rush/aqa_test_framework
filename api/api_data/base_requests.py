4bedab8724d50a4249ef72ef4a9696f92fbb29ae
Rizvanov <SaintFather89@yandex.ru>
2023-04-10 19:52:02 +0300

    fix code

--- api/api_data/base_requests.py
+++ api/api_data/base_requests.py
@@ -4,18 +4,23 @@ from requests import Response
 
 class BaseRequests:
 
-    def get(self, url: str, params: dict = None) -> Response:
+    @staticmethod
+    def get(url: str, params: dict = None) -> Response:
         return requests.request("GET", url=url, params=params)
 
-    def patch(self, url: str, json: dict = None) -> Response:
+    @staticmethod
+    def patch(url: str, json: dict = None) -> Response:
         return requests.request("PATCH", url=url, json=json)
 
-    def post(self, url: str, json: dict = None) -> Response:
+    @staticmethod
+    def post(url: str, json: dict = None) -> Response:
         return requests.request("POST", url=url, json=json)
 
-    def put(self, url: str, json: dict = None) -> Response:
+    @staticmethod
+    def put(url: str, json: dict = None) -> Response:
         return requests.request("PUT", url=url, json=json)
 
-    def delete(self, url: str) -> Response:
+    @staticmethod
+    def delete(url: str) -> Response:
         return requests.request("DELETE", url=url)
 
